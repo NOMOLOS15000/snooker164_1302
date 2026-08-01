c44c8d6b279d88dec116f38ea7cdc5cee9046584
NOMOLOS15000 <titaya151014@gmail.com>
2026-08-01 23:50:21 +0700

    BallPosition&BallPrefab Finish

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,7 +21,14 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        SetBall(BallColor.White, 0);
         SetBall(BallColor.Red, 1);
+        SetBall(BallColor.Yellow, 2);
+        SetBall(BallColor.Green, 3);
+        SetBall(BallColor.Brown, 4);
+        SetBall(BallColor.Blue, 5);
+        SetBall(BallColor.Pink, 6);
+        SetBall(BallColor.Black, 7);
     }
 
     // Update is called once per frame
@@ -32,6 +39,13 @@ public class GameManager : MonoBehaviour
 
     private void SetBall(BallColor col, int i)
     {
+        // เช็กป้องกันถ้า index เกินขนาด Array
+        if (i >= ballPrefab.Length || i >= ballPositions.Length)
+        {
+            Debug.LogError($"Index {i} เกินขนาด Array! Check Inspector (ballPrefab size: {ballPrefab.Length}, ballPositions size: {ballPositions.Length})");
+            return;
+        }
+
         GameObject obj = Instantiate(ballPrefab[i],
                     ballPositions[i].transform.position,
                     Quaternion.identity);
